5260cce49b491e6f2a6e28c30053adfa4e69805b
jayapriyamano31@gmail.com <jayapriyamano31@gmail.com>
2024-05-15 13:15:59 +0530

    changes in

--- server/server.js
+++ server/server.js
@@ -26,23 +26,23 @@ client.query('SELECT NOW()')
     
     // Upsert API for users_profiles
     app.post('/api/users_profiles/upsert', async (req, res) => {
-        const { id, name, email, phone_no,  is_active, password, role,created_at ,updated_at  } = req.body;
+        const { id, name, email_id, phone_no, password,created_at ,updated_at,role,is_active } = req.body;
         try {
             if (id?.length > 0) {
                 // If the profile exists, perform a partial update
                 const updatedProfile = await client.query(
                     `UPDATE users_profiles
                      SET name = COALESCE($2, name),
-                         email = COALESCE($3, email),
+                         email_id = COALESCE($3, email_id),
                          phone_no = COALESCE($4, phone_no),
-                         is_active = COALESCE($6, is_active),
-                         password = COALESCE($7, password),
-                         role = COALESCE($8, role),
-                         created_at  = COALESCE($9, created_at ),
-                         updated_at  = COALESCE($10, updated_at )
+                         password = COALESCE($6, password),
+                         created_at = COALESCE($7, created_at),
+                         updated_at = COALESCE($8, updated_at),
+                         role = COALESCE($9, role ),
+                         is_active  = COALESCE($10, is_active )
                      WHERE id = $1
                      RETURNING *;`,
-                    [id, name, email, phone_no,  is_active, password, role, created_at ,updated_at]
+                    [id, name, email_id, phone_no, password,created_at ,updated_at,role,is_active]
                 );
     
                 res.json(updatedProfile.rows[0]);
@@ -50,10 +50,10 @@ client.query('SELECT NOW()')
             } else {
                 // If the profile doesn't exist, insert a new one
                 const result = await client.query(
-                    `INSERT INTO users_profiles (id, name, email, phone_no,  is_active, password, role,created_at ,updated_at )
+                    `INSERT INTO users_profiles (id, name, email_id, phone_no,password,created_at ,updated_at,role,is_active )
                      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
                      RETURNING *;`,
-                    [uuidv4(), name, email, phone_no,  is_active, password, role, created_at ,updated_at]
+                    [uuidv4(), name, email_id, phone_no, password,created_at ,updated_at,role,is_active]
                 );
     
                 res.json(result.rows[0]);
@@ -68,7 +68,7 @@ client.query('SELECT NOW()')
     
     // Upsert API for projects
     app.post('/api/projects/upsert', async (req, res) => {
-        const { id, name, description, is_active, project_status, start_date, end_date ,created_at,updated_at ,team_members } = req.body;
+        const { id, name, description,start_date,end_date ,project_status ,created_at,updated_at,team_manager,is_active } = req.body;
     
         try {
             if (id?.length > 0) {
@@ -78,16 +78,16 @@ client.query('SELECT NOW()')
                     `UPDATE projects
                      SET name = COALESCE($2, name),
                          description = COALESCE($3, description),
-                         is_active = COALESCE($4, is_active),
-                         project_status = COALESCE($5, project_status),
-                         start_date = COALESCE($6, start_date),
-                         end_date = COALESCE($7, end_date),
-                         created_at = COALESCE($8, created_at),
-                         updated_at = COALESCE($9, updated_at),
-                         team_members= COALESCE($10, team_members)
+                         start_date = COALESCE($4, start_date),
+                         end_date = COALESCE($5, end_date),
+                         project_status = COALESCE($6, project_status),
+                         created_at = COALESCE($7, created_at),
+                         updated_at = COALESCE($8, updated_at),
+                         team_manager = COALESCE($9, team_manager),
+                         is_active = COALESCE($10, is_active),
                      WHERE id = $1
                      RETURNING *;`,
-                    [id, name, description, is_active, project_status, start_date, end_date ,created_at,updated_at ,team_members]
+                    [id, name, description, start_date,end_date ,project_status , created_at,updated_at,team_manager,is_active]
                 );
     
                 res.json(updatedProject.rows[0]);
@@ -95,13 +95,12 @@ client.query('SELECT NOW()')
             } else {
                 // If the project doesn't exist, insert a new one
                 const result = await client.query(
-                    `INSERT INTO projects (id, name, description, is_active, project_status, start_date, end_date ,created_at,updated_at ,team_members
-                    )
+                    `INSERT INTO projects (id, name, description, start_date,end_date ,project_status ,created_at,updated_at,team_manager,is_active)
                      VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9,$10)
                      RETURNING *;`,
-                    [uuidv4(), name, description, is_active, project_status, start_date, end_date ,created_at,updated_at ,team_members]
+                    [uuidv4(), name, description,start_date,end_date ,project_status ,created_at,updated_at,team_manager,is_active]
                 );
-    
+
                 res.json(result.rows[0]);
             }
         } catch (error) {
@@ -112,7 +111,7 @@ client.query('SELECT NOW()')
     
     // Upsert API for tasks
     app.post('/api/tasks/upsert', async (req, res) => {
-        const { id, task_id,user_id,doubt_message,resolved,resolved_by,resolved_at} = req.body;
+        const { id, name,project_id,description,start_date,end_date,task_priority,assigned_id,created_at,updated_at,is_active} = req.body;
     
         try {
             if (id?.length > 0) {
@@ -120,19 +119,19 @@ client.query('SELECT NOW()')
                 const updatedTask = await client.query(
                     `UPDATE tasks
                      SET name = COALESCE($2, name),
-                         description = COALESCE($3, description),
-                         is_active = COALESCE($4, is_active),
-                         start_date = COALESCE($5, status),
-                         assignee_id = COALESCE($6, assignee_id),
-                          end_date= COALESCE($7,end_date ),
-                         project_id = COALESCE($8, project_id),
+                         project_id = COALESCE($3, project_id),
+                         description = COALESCE($4, description),
+                         start_date = COALESCE($5, start_date),
+                         end_date = COALESCE($6, end_date),
+                          task_priority= COALESCE($7,priority ),
+                         assigned_id = COALESCE($8, assigned_id),
                          created_at = COALESCE($9, created_at),
-                         updated_at = COALESCE($810, updated_at),
-                         task_priority = COALESCE($11, task_priority),
+                         updated_at = COALESCE($10, updated_at),
+                         is_active = COALESCE($11, is_active),
 
                      WHERE id = $1
                      RETURNING *;`,
-                    [id, name, description, is_active,start_date, assignee_id, end_date, project_id ,created_at,updated_at,task_priority ]
+                    [id, name, project_id,description,start_date,end_date,task_priority,assigned_id,created_at,updated_at,is_active]
                 );
     
                 res.json(updatedTask.rows[0]);
@@ -155,7 +154,7 @@ client.query('SELECT NOW()')
     });
     // Upsert API for notifications
     app.post('/api/notifications/upsert', async (req, res) => {
-        const { id, name, description, is_active, notification_status, send_to ,created_at,updated_at ,is_read } = req.body;
+        const { id, name, send_to,description, notification_status,task_id,notification_date,is_active,is_read } = req.body;
     
         try {
             if (id?.length > 0) {
@@ -163,16 +162,16 @@ client.query('SELECT NOW()')
                 const updatednotifications = await client.query(
                     `UPDATE notifications
                      SET name = COALESCE($2, name),
-                         description = COALESCE($3, description),
-                         is_active = COALESCE($4, is_active),
+                         send_to = COALESCE($3, send_to),
+                         description = COALESCE($4, description),
                          notifications_status = COALESCE($5, notifications_status),
-                         send_to = COALESCE($6, send_to),
-                         created_at = COALESCE($7,created_at),
-                         updated_at = COALESCE($8, updated_at),
+                         task_id = COALESCE($6, task_id),
+                         notification_date = COALESCE($7,notification_date),
+                         is_active = COALESCE($8, is_active),
                          is_read= COALESCE($9,is_read),
                      WHERE id = $1
                      RETURNING *;`,
-                    [id, name, description, is_active, notification_status, send_to ,created_at,updated_at ,is_read]
+                    [id, name, send_to,description, notification_status,task_id,notification_date,is_active,is_read]
                 );
     
                 res.json(updatednotifications.rows[0]);
@@ -180,13 +179,12 @@ client.query('SELECT NOW()')
             } else {
                 // If the noupdatednotifications doesn't exist, insert a new one
                 const result = await client.query(
-                    `INSERT INTO noupdatednotifications (id, name, description, is_active, notification_status, send_to ,created_at,updated_at ,is_read]
-                    );)
+                    `INSERT INTO noupdatednotifications (id, name, send_to,description, notification_status,task_id,notification_date,is_active,is_read]
                      VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9,)
                      RETURNING *;`,
-                    [uuidv4(), name, description, is_active, notification_status, send_to ,created_at,updated_at ,is_read]
-                );
-                
+                    [uuidv4(), name, send_to,description, notification_status,task_id,notification_date,is_active,is_read]
+                    
+                    );
     
                 res.json(result.rows[0]);
             }
@@ -255,33 +253,35 @@ client.query('SELECT NOW()')
     })
     // Upsert API for task_doubts
     app.post('/api/task_doubts/upsert', async (req, res) => {
-        const { id, task_id,user_id,doubt_message,resolved,resolved_by,resolved_at} = req.body;
+        const { id, task_id,user_id,doubt_message,resolved,resolved_by,resolved_date,raised_date} = req.body;
     
         try {
             if (id?.length > 0) {
                 // If the task_doubts exists, perform a partial update
-                const updatedTask_dobts = await client.query(
+                const updatedTask_doubts = await client.query(
                     `UPDATE task_doubts
                      SET task_id = COALESCE($2, task_id),
                          user_id = COALESCE($3, user_id),
                          doubt_message = COALESCE($4, doubt_message),
                          resolved = COALESCE($5, resolved),
                          resolved_by = COALESCE($6, resolved_by),
-                          resolved_at= COALESCE($7,resolved_at ),
+                          resolved_date= COALESCE($7,resolved_date ),
+                          raised_date= COALESCE($7,raised_date ),
                      WHERE id = $1
                      RETURNING *;`,
-                    [id, task_id, user_id, doubt_message,resolved,resolved_by,resolved_at ]
+                    [id, task_id, user_id, doubt_message,resolved,resolved_by,resolved_date,raised_date ]
                 );
     
-                res.json(updatedTask_dobts.rows[0]);
+                res.json(updatedTask_doubts.rows[0]);
     
             } else {
                 // If the task doesn't exist, insert a new one
                 const result = await client.query(
-                    `INSERT INTO tasks (id, task_id,user_id,doubt_message,resolved,resolved_by,resolved_at)
-                     VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9, $10, $11)
+                    `INSERT INTO tasks (id, task_id,user_id,doubt_message,resolved,resolved_by,resolved_date,raised_date)
+                     VALUES ($1, $2, $3, $4, $5, $6, $7, $8,)
                      RETURNING *;`,
-                    [uuidv4(), task_id, description, is_active,start_date, assignee_id, end_date, project_id ,created_at,updated_at,task_priority ]
+                    [uuidv4(), task_id, user_id, doubt_message,resolved,resolved_by,resolved_date,raised_date ]
+        
                 );
     
                 res.json(result.rows[0]);
